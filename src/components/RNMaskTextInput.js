04458a1ed8b94513ac3290487926b8ace78a51f0
MD ABS Jabed <absj4k@gmail.com>
2021-02-01 02:57:57 +0600

    add task ui WIP...

---
+++ src/components/RNMaskTextInput.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {TextInputMask} from 'react-native-masked-text'
+const RNMaskTextInput = (props) => {
+  return (
+    <View
+      style={{
+        marginTop: 10,
+        //borderColor: '#007FFF',
+        borderBottomColor: '#f2f2f5',
+        borderBottomWidth: 1,
+      }}>
+      <Text style={{fontSize:12, fontWeight:'bold', color:'#c3c3c5', letterSpacing: .3}}>{props.labelName}</Text>
+      <TextInputMask
+        defaultValue={props.defaultValue}
+        type={props.type}
+        options={props.options}
+        keyboardType={props.keyboardType}
+        placeholder={props.placeholder}
+        placeholderTextColor={props.placeholderTextColor}
+        value={props.value}
+        onChangeText={props.onChangeText}
+        style={props.style}
+        />
+    </View>
+  );
+};
+
+export default RNMaskTextInput;
--- src/screens/AddNewScreen.js
+++ src/screens/AddNewScreen.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
 import { Text, View, Dimensions, StyleSheet, SafeAreaView } from 'react-native'
 import Icon from 'react-native-vector-icons/AntDesign';
+import moment from 'moment'
 import {COLORS} from '../styles/colors'
+import IIcon from 'react-native-vector-icons/Ionicons';//md-notifications-outline  md-location-outline https://www.npmjs.com/package/react-native-material-color-picker
 import RNTextInput from '../components/RNTextInput'
+import RNMaskTextInput from '../components/RNMaskTextInput'
 const screenWidth = Math.round(Dimensions.get('window').width);
 import { handleAndroidBackButton, removeAndroidBackButtonHandler } from '../utils/backHandler.config';
 
@@ -10,10 +13,15 @@ export class AddNewScreen extends Component {
 
     
     state={
-        fullName: "fullname",
-        email:"email",
-        password: "pass",
-        birthday:""
+        title: "fullname",
+        description:"email",
+        date: "pass",
+        fromTime:"",
+        toTime:"",
+        location:'',
+        notifyTime: '',
+        labelColor:'',
+        dt:''
     }
 
 
@@ -33,7 +41,7 @@ export class AddNewScreen extends Component {
     render() {
         return (
             <View style={style.container}>
-                <View style={{flex:.11, width: screenWidth, flexDirection:'column'}}>
+                <View style={{flex:.1, width: screenWidth, flexDirection:'column'}}>
                     <View style={{flex:1, flexDirection:'row', paddingTop: 18, paddingLeft:10, paddingRight:10, justifyContent:'space-between'}}>
                         <View>
                             <Icon onPress={()=> this.props.navigation.goBack()} name="close" size={30} color="#c6c6c7" />
@@ -51,43 +59,87 @@ export class AddNewScreen extends Component {
                     <View style={{flex: 1, flexDirection:'column', backgroundColor: 'white'}}>
                         <View style={{flex: 1, flexDirection:'column', width: screenWidth}}>
                             <View style={{backgroundColor:'#f8f8f9',width: screenWidth}}>
-                                <RNTextInput
-                                    style={{}}
-                                    onChangeText={(fullName) => this.setState({fullName})}
-                                    labelName="TITLE"
-                                />
-                                <RNTextInput
-                                    onChangeText={(email) => this.setState({email})}
-                                    labelName="DESCRIPTION"
-                                    maxLength={20}
-                                />
+                                <View style={{marginLeft: 20, marginRight:20}}>
+                                    <RNTextInput
+                                        style={{color:'#66666a'}}
+                                        onChangeText={(title) => this.setState({title})}
+                                        labelName="TITLE"
+                                    />
+                                    <RNTextInput
+                                        style={{color:'#66666a'}}
+                                        onChangeText={(description) => this.setState({description})}
+                                        labelName="DESCRIPTION"
+                                    />
+                                </View>
                             </View>
                             <View style={{width: screenWidth}}>
-                                <RNTextInput
-                                    onChangeText={(password) => this.setState({password})}
+                                <View style={{marginLeft: 20, marginRight:20}}>
+                                    
+                                <RNMaskTextInput
                                     labelName="DATE"
-                                />
-                                <RNTextInput
-                                    onChangeText={(password) => this.setState({password})}
-                                    labelName="FROM/TO"
-                                />
-                                <RNTextInput
-                                    //placeholder="e.g: May 15, 1993"
-                                    onChangeText={(birthday) => this.setState({birthday})}
-                                    labelName="LOCATION"
-                                />
-                                
-                                <RNTextInput
-                                    //placeholder="e.g: May 15, 1993"
-                                    onChangeText={(birthday) => this.setState({birthday})}
-                                    labelName="NOTIFY"
-                                />
-                                
-                                <RNTextInput
-                                    //placeholder="e.g: May 15, 1993"
-                                    onChangeText={(birthday) => this.setState({birthday})}
-                                    labelName="LABEL"
-                                />
+                                    type={'datetime'}
+                                    options={{
+                                        format: 'YYYY-MM-DD'
+                                      }}
+                                    placeholder={moment().format('YYYY-MM-DD')}
+                                    placeholderTextColor="#25be7b"
+                                    value={this.state.date}
+                                    onChangeText={(date) => this.setState({date})}
+                                    style={{color:'#66666a'}}
+                                    />
+                                    <View >
+                                        <View style={{flexDirection:'row', justifyContent:'space-between'}}>
+                                            <RNMaskTextInput
+                                            labelName="FROM"
+                                            type={'datetime'}
+                                            options={{
+                                                format: 'HH:mma'
+                                            }}
+                                            keyboardType="numbers-and-punctuation"
+                                            placeholder={moment().format('HH:mma')}
+                                            placeholderTextColor="#25be7b"
+                                            value={this.state.fromTime}
+                                            onChangeText={(fromTime) => this.setState({fromTime})}
+                                            style={{color:'#66666a', width: screenWidth*.40}}
+                                            />
+                                            <RNMaskTextInput
+                                            labelName="TO"
+                                            type={'datetime'}
+                                            options={{
+                                                format: 'HH:mma'
+                                            }}
+                                            keyboardType="numbers-and-punctuation"
+                                            placeholder={moment().format('HH:mma')}
+                                            placeholderTextColor="#25be7b"
+                                            value={this.state.toTime}
+                                            onChangeText={(toTime) => this.setState({toTime})}
+                                            style={{color:'#66666a', width: screenWidth*.40}}
+                                            />
+                                        </View>
+                                    </View>
+                                    
+                                    <RNTextInput
+                                        style={{color:'#66666a'}}
+                                        //placeholder="e.g: May 15, 1993"
+                                        onChangeText={(location) => this.setState({location})}
+                                        labelName="LOCATION"
+                                    />
+                                    
+                                    <RNTextInput
+                                        style={{color:'#66666a'}}
+                                        //placeholder="e.g: May 15, 1993"
+                                        onChangeText={(notifyTime) => this.setState({notifyTime})}
+                                        labelName="NOTIFY"
+                                    />
+                                    
+                                    <RNTextInput
+                                        style={{color:'#66666a'}}
+                                        //placeholder="e.g: May 15, 1993"
+                                        onChangeText={(labelColor) => this.setState({labelColor})}
+                                        labelName="LABEL"
+                                    />
+                                    
+                                </View>
                             </View>
                         </View>
                     </View>
@@ -106,7 +158,17 @@ const style = StyleSheet.create({
         flexDirection: 'column',
         justifyContent: 'space-between',
         alignItems:'center'
-    }
+    },
+    textInputStype :{
+        // height: 50,
+        // width: '100%',
+        // borderColor: 'gray',
+        // borderWidth: 1
+        marginTop: 10,
+        //borderColor: '#007FFF',
+        borderBottomColor: '#f2f2f5',
+        borderBottomWidth: 1,
+      }
   });
 
 
